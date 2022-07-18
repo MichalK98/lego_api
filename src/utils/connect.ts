8d245c21a979556ab8d92be3fc2e293860e0df67
MichalK98 <michal10203040@gmail.com>
2022-07-18 11:48:19 +0200

    Implement logger middleware

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,14 +1,15 @@
 import mongoose from 'mongoose';
 import config from 'config';
+import log from './logger';
 
 async function connect() {
   const dbUri = config.get<string>('dbUri');
 
   try {
     await mongoose.connect(dbUri);
-    console.log('Database connected');
+    log.info('Database connected');
   } catch (e) {
-    console.log('Could not connect to the database');
+    log.error('Could not connect to the database');
   }
 }
 
