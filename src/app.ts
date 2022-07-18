b79073c4e1697d8f647cfabaffbbbb6643be6e07
MichalK98 <michal10203040@gmail.com>
2022-07-18 09:39:02 +0200

    Implement express

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log('Lego!');
+import express from 'express';
+import config from 'config';
+import routes from './routes';
+
+const port = config.get<number>('port');
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+
+  routes(app);
+});
