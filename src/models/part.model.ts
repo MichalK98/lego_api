c1f7984af6536726e9bf6467b7d4f597872d95ce
MichalK98 <michal10203040@gmail.com>
2022-07-18 09:56:16 +0200

    Add Part Model

---
+++ src/models/part.model.ts
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+import { v4 as uuid } from 'uuid';
+
+export interface PartInput {
+  title: string;
+  image: string;
+}
+
+export interface PartDocument extends PartInput, mongoose.Document {
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const partSchema = new mongoose.Schema(
+  {
+    partId: {
+      type: String,
+      require: true,
+      unique: true,
+      default: () => `part_${uuid()}`
+    },
+    title: { type: String, require: true },
+    image: { type: String, require: true }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const partModel = mongoose.model<PartDocument>('Part', partSchema);
+
+export default partModel;
