60504a4f830ec0a3789ad2e055ba41f14f414f94
MichalK98 <michal10203040@gmail.com>
2022-07-18 11:39:22 +0200

    Implement scheme validation middleware

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,13 @@
 import { Express, Request, Response } from 'express';
+import validateResource from './middleware/validateResource';
 import { CreatePartHandler } from './controller/part.controller';
+import { createPartSchema } from './schema/part.schema';
 
 function routes(app: Express) {
   app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200));
 
   /* Part Routes */
-  app.post('/api/part', CreatePartHandler);
+  app.post('/api/part', validateResource(createPartSchema), CreatePartHandler);
 }
 
 export default routes;
