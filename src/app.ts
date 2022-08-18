49b6a9f76cfd7118f5ef83602ad8a65d4eb83239
MichalK98 <michal10203040@gmail.com>
2022-08-18 15:10:39 +0200

    Implement Auth0

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
+import 'dotenv/config';
 import express from 'express';
+import { auth } from 'express-openid-connect';
 import config from 'config';
 import routes from './routes';
 import connect from './utils/connect';
@@ -9,6 +11,17 @@ const port = config.get<number>('port');
 const app = express();
 
 app.use(express.json());
+
+app.use(
+  auth({
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.ISSUER_BASE_URL
+  })
+);
 
 app.listen(port, async () => {
   logger.info(`Server running at http://localhost:${port}`);
--- src/routes.ts
+++ src/routes.ts
@@ -7,13 +7,23 @@ import {
   updatePartHandler
 } from './controller/part.controller';
 import { createPartSchema, updatePartSchema } from './schema/part.schema';
+import { requiresAuth } from 'express-openid-connect';
 
 function routes(app: Express) {
   /* Part Routes */
   app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200));
 
+  /* Auth */
+  app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  });
+
   /* Part Routes */
-  app.post('/api/parts', validateResource(createPartSchema), createPartHandler);
+  app.post(
+    '/api/parts',
+    [validateResource(createPartSchema), requiresAuth()],
+    createPartHandler
+  );
 
   app.get('/api/parts', readPartHandler);
 
